d71f348df26719c91ebee0ffd05e2990dc25d1b9
Chen Barsano <chenbarsano@gmail.com>
2025-02-04 16:19:06 +0200

    signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,12 @@
 import User from "../models/userModel.js"
+import jwt from 'jsonwebtoken'
+
+const createToken = (_id) => {
+    // the _id is part of the payload of the token
+    // and then we pass in the secret 
+    // and expiresIn which means the user will remain logged in for 3 days
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 // login user
 export const loginUser = async (req, res) => {
@@ -10,7 +18,11 @@ export const signupUser = async (req, res) => {
     const {email, password} = req.body
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
