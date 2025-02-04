40dce323553fef30080a06cf0e22c825a33606fb
Chen Barsano <chenbarsano@gmail.com>
2025-02-04 17:10:52 +0200

    logging users in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,4 +45,24 @@ userSchema.statics.signup = async function(email, password) {
 
 }
 
+// static login method (using 'function' and not an arrow so we can use 'this')
+userSchema.statics.login = async function(email, password) {
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({email})
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+
+    // passing the plaintext password, and the hashed user password
+    const match = await bcrypt.compare(password, user.password)
+    if(!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
+
 export default mongoose.model('User', userSchema)
